933275fdd0028eb7503ba944f7d36f21e0b31213
AstroPolly <PollyDolly706@gmail.com>
2023-12-19 17:22:13 +0300

    Labs

--- LAB_2/lab2.py
+++ LAB_2/lab2.py
@@ -11,44 +11,37 @@ class SearchBody(BaseModel):
     title: str
     sym_count: int
 
-"""@app.get("/wiki/search_by_path/{title}")
-def getByPath(title):
+@app.get("/wiki/search_by_path/{title}")
+def getByPath(title: str):
     try:
-        return {"result": wikipedia.search(title).content[120],
-                "title": wikipedia.page(title).title,
-                "summary":wikipedia.search(title).content[:60] }
+        return {"result": wikipedia.page(title).content,
+                "title": wikipedia.page(title).title}
     except PageError:
         raise HTTPException(status_code=404)
     except DisambiguationError:
-        raise {"result": wikipedia.search(title).content[120],
-                "title": wikipedia.page(title).title,
-                "summary":wikipedia.search(title).content[:60] }
+        raise {"result": "Пиши нормально"}
+
 
 @app.get("/wiki/search_by_query")
-def getByQuery(title: str, sym_count: int):
+def getByQuery(title: str):
     try:
-        return {"result": wikipedia.page(title).content}
+        return { "title": wikipedia.page(title).title,
+            "result": wikipedia.page(title).content}
     except PageError:
         raise HTTPException(status_code=404)
     except DisambiguationError:
-        raise {"result": wikipedia.search(title).content[120],
-                "title": wikipedia.page(title).title,
-                "summary":wikipedia.search(title).content[:60] }
+        raise {"result": "Пиши нормально"}
 
 @app.post("/wiki/search_by_body")
 def postByBody(body: SearchBody):
     try:
-        return {"result": wikipedia.search(SearchBody.title).content[SearchBody.sym_count],
-                "title": wikipedia.page(SearchBody.title).title,
-                "summary": wikipedia.search(SearchBody.title).content[SearchBody.sym_count]}
+        return {"result": wikipedia.search(body.title).content[body.sym_count],
+                "title": wikipedia.page(body.title).title,
+                "summary": wikipedia.page(body.title).content[body.sym_count]}
     except PageError:
         raise HTTPException(status_code=404)
     except DisambiguationError:
-        raise {"result": "Пиши нормально"}"""
-
-@app.post("/", response_model=Joke)
-def create_joke(joke_input: JokeInput):
-    return Joke(friend=joke_input.friend, joke=pyjokes.get_joke())
+        raise {"result": "Пиши нормально"}
 
 
 if __name__ == "__lab2__":
